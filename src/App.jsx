ff765d92ce80e4aa3971a7937826d7e32872da5a
Augusto Lázaro <augusto.lr06@gmail.com>
2022-12-22 16:29:02 -0300

    Initial commit: Project already started

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Post } from "./Post"
+
+
+import "./style.css"
+
+
+export function App() {
+
+  return (
+
+    <div>
+      <Post 
+        author="Augusto Lázaro"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum. Incidunt voluptates ex error, illo soluta harum laudantium magni minima voluptatem quas quam! Voluptas magni enim ad distinctio est! Quas?"
+      />
+
+      <Post
+        author="Aline Ferreira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta voluptas esse, libero maxime aliquid aut magnam ullam vitae repellat eaque distinctio perferendis illo dolorem ad, dignissimos unde tenetur maiores?"
+      />
+    </div>
+
+  )
+
+}
+
+
+
+
+
+
+
---
+++ src/Post.jsx
@@ -0,0 +1,16 @@
+
+
+export function Post(props){
+
+
+    return(
+        <div>
+            <h1>{props.author}</h1>
+            <p>{props.content}</p>
+        </div>
+    )
+
+}
+
+
+
