d529d89e030bec1a0d04c3f474ac5ad2dcc5cabe
Augusto Lázaro <augusto.lr06@gmail.com>
2022-12-26 16:52:02 -0300

    Refactor: replace Post and refactor imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header/Header"
-import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar/Sidebar"
+import { Post } from "./components/Post/Post"
 
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from "./components/Sidebar/Sidebar"
 
 
 export function App() {
@@ -16,17 +16,10 @@ export function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <Post 
-          author="Augusto Lázaro"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum. Incidunt voluptates ex error, illo soluta harum laudantium magni minima voluptatem quas quam! Voluptas magni enim ad distinctio est! Quas?"
-        />
 
-        <Post
-          author="Aline Ferreira"
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta voluptas esse, libero maxime aliquid aut magnam ullam vitae repellat eaque distinctio perferendis illo dolorem ad, dignissimos unde tenetur maiores?"
-        />
       </main>
     </div>
+    
     </div>
 
   )
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,7 @@
+import styles from './Post.module.css'
+
+export function Post(){
+    return(
+        <article className={styles.post}></article>
+    )
+}
