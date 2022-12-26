82263057eb4eb69e16ade89b91c131608bd75d7f
Augusto Lázaro <augusto.lr06@gmail.com>
2022-12-26 11:45:54 -0300

    chore: prepare area for sidebar and posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Header } from "./components/Header/Header"
 import { Post } from "./Post"
 
 import './global.css'
+import styles from './App.module.css'
 
 
 export function App() {
@@ -11,23 +12,25 @@ export function App() {
     <div>
       <Header />
 
-      <Post 
-        author="Augusto Lázaro"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum. Incidunt voluptates ex error, illo soluta harum laudantium magni minima voluptatem quas quam! Voluptas magni enim ad distinctio est! Quas?"
-      />
+    <div className={styles.wrapper}>
+      <aside>sidebar</aside>
+      <main>
+        <Post 
+          author="Augusto Lázaro"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum. Incidunt voluptates ex error, illo soluta harum laudantium magni minima voluptatem quas quam! Voluptas magni enim ad distinctio est! Quas?"
+        />
 
-      <Post
-        author="Aline Ferreira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta voluptas esse, libero maxime aliquid aut magnam ullam vitae repellat eaque distinctio perferendis illo dolorem ad, dignissimos unde tenetur maiores?"
-      />
+        <Post
+          author="Aline Ferreira"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta voluptas esse, libero maxime aliquid aut magnam ullam vitae repellat eaque distinctio perferendis illo dolorem ad, dignissimos unde tenetur maiores?"
+        />
+      </main>
+    </div>
     </div>
 
   )
 
 }
-
-
-
 
 
 
