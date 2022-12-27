58c46a375d445c17fa94269a2feca8c49265f1f4
Augusto Lázaro <augusto.lr06@gmail.com>
2022-12-27 14:59:28 -0300

    fetat: ensure Comment component

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,32 @@
+import {ThumbsUp, Trash} from "phosphor-react"
+
+import styles from './Comment.module.css'
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/augustolr06.png" />
+            <div className={styles.commentBox}>
+
+                <div className={styles.commentContent}>
+                    <header className={styles.commentHeader}>
+                        <div className={styles.authorAndTime}>
+                            <strong>Augusto Lázaro</strong>
+                            <time title='27 de Dezembro de 2022 às 10:14h' dateTime='2022-12-26 17:00:02'>Cerca de 2h</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom Augusto, parabéns!! 👏👏</p>
+                </div>
+
+                <button className={styles.applaud}>
+                    <ThumbsUp size={20} />Aplaudir • 02
+                    </button>
+            </div>
+        </div>
+    )
+}
